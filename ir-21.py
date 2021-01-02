bcbfb6ba49404d001feb68c4a80cfc1fcbc7f120
TheOrangeWizard <theorangewizard@protonmail.com>
2021-01-02 22:43:46 +1300

    test

--- ir-21.py
+++ ir-21.py
@@ -135,43 +135,10 @@ def on_player_list_item(player_list_item_packet):
         print(e)
 
 
-class raw(object):
-    def __init__(self, stream):
-        self.stream = stream
-        self.fd = self.stream.fileno()
-
-    def __enter__(self):
-        self.original_stty = termios.tcgetattr(self.stream)
-        tty.setcbreak(self.stream)
-
-    def __exit__(self, type, value, traceback):
-        termios.tcsetattr(self.stream, termios.TCSANOW, self.original_stty)
-
-
-class nonblocking(object):
-    def __init__(self, stream):
-        self.stream = stream
-        self.fd = self.stream.fileno()
-
-    def __enter__(self):
-        self.orig_fl = fcntl.fcntl(self.fd, fcntl.F_GETFL)
-        fcntl.fcntl(self.fd, fcntl.F_SETFL, self.orig_fl | os.O_NONBLOCK)
-
-    def __exit__(self, *args):
-        fcntl.fcntl(self.fd, fcntl.F_SETFL, self.orig_fl)
-
-
 if __name__ == "__main__":
     print(dtstring(), "starting up")
     a = time.time()
     connection.auth_token.authenticate(config.username, config.password)
     connection.connect()
-    with raw(sys.stdin):
-        with nonblocking(sys.stdin):
-            while True:
-                c = sys.stdin.read(1)
-                if c:
-                    print(repr(c))
-                else:
-                    print('not ready')
-                time.sleep(.1)
+    while True:
+        time.sleep(60)
