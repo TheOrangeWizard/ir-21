4a6227215adfc4b70448b165e9e61b801ac3d347
TheOrangeWizard <theorangewizard@protonmail.com>
2021-01-04 22:53:55 +1300

    test14

--- ir-21.py
+++ ir-21.py
@@ -1,9 +1,4 @@
 import config
-import commands
-
-from commands import timestring
-from commands import datestring
-from commands import dtstring
 
 import json
 import time
@@ -17,6 +12,21 @@ from minecraft import authentication
 from minecraft.exceptions import YggdrasilError
 from minecraft.networking.connection import Connection
 from minecraft.networking import packets
+
+
+def timestring():
+    mtime = datetime.datetime.utcnow()
+    return "[{:%H:%M:%S}]".format(mtime)
+
+
+def datestring():
+    mtime = datetime.datetime.utcnow()
+    return "[{:%d/%m/%y}]".format(mtime)
+
+
+def dtstring():
+    mtime = datetime.datetime.utcnow()
+    return datestring() + " " + timestring()
 
 
 def parse(obj):
@@ -107,6 +117,52 @@ def parse_commands():
             commands.commands[cmd](txt)
         except Exception as e:
             print(str(type(e)) + ": " + str(e))
+
+
+commands = {}
+
+
+def command(cmd):
+    commands[cmd.__name__] = cmd
+
+
+@command
+def run(txt):
+    try:
+        exec(txt)
+    except Exception as e:
+        print(str(type(e)) + ": " + str(e))
+
+
+@command
+def say(txt):
+    try:
+        send_chat(txt)
+    except Exception as e:
+        print(type(e) + ": " + str(e))
+
+
+@command
+def status(txt):
+    print(dtstring(), "ir-21 status:")
+    print("account name:", connection.auth_token.profile.name)
+    print("server address:", connection.host, connection.port)
+    print("connection active:", connection.connected)
+    print("spawned:", connection.spawned)
+    print("reactor type:", type(connection.reactor))
+
+
+@command
+def login(txt):
+    connection.connect()
+
+
+@command
+def logout(txt):
+    if txt in ["now", "force", "f"]:
+        connection.disconnect()
+    else:
+        send_chat("/logout")
 
 
 def background():
